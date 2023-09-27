559e670718c5fcc8406f445dffd1c8c835495fc3
Adarsh Jaiswal <adarsh.synchsofthq@gmail.com>
2023-09-27 22:17:13 +0530

    show fetched images in card

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useState } from "react";
-import { PuffLoader } from "react-spinners"; // Importing the loader from react-spinners
+import React, { useState, useEffect } from "react";
+import { PuffLoader } from "react-spinners";
 
 function App() {
-  const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   const getUsers = async () => {
-    setIsLoading(true); // Set isLoading to true when fetching data
+    setIsLoading(true);
 
     try {
       const response = await fetch("https://reqres.in/api/users?page=1");
       if (response.ok) {
         const data = await response.json();
-        setUserData(data);
+        setUserData(data.data); // Assuming data is an array of user objects
       } else {
         console.error("Failed to fetch data");
       }
     } catch (error) {
       console.error("Error:", error);
     } finally {
-      setIsLoading(false); // Set isLoading to false when data fetching is complete
+      setIsLoading(false);
     }
   };
 
@@ -32,35 +36,40 @@ function App() {
           href="/"
           className="d-flex align-items-center mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
         >
-          <img src={logo} className="logo" alt="logo"  />
+          <img src={logo} className="logo" alt="logo" />
           <span className="fs-4 text-black">logo</span>
         </a>
 
         <ul className="nav nav-pills">
           <li className="nav-item">
-            <a
-              href="#"
-              className="nav-link active"
-              aria-current="page"
-              onClick={getUsers}
-            >
+            <a href="#" className="nav-link active" aria-current="page" onClick={getUsers}>
               Get Users
             </a>
           </li>
         </ul>
       </header>
 
-      {isLoading ? ( // Conditional rendering based on the isLoading state
-        <div class="d-flex justify-content-center min-vh-100">
+      {isLoading ? (
+        <div className="d-flex justify-content-center min-vh-100">
           <PuffLoader color="blue" />
         </div>
       ) : (
-        userData && (
-          <div className="container">
-            <h2>Fetched User Data</h2>
-            <pre>{JSON.stringify(userData, null, 2)}</pre>
+        <div className="container">
+          <h2>Fetched User Data</h2>
+          <div className="row">
+            {userData.map((user) => (
+              <div key={user.id} className="col-md-4">
+                <div className="card mb-3">
+                  <img src={user.avatar} className="card-img-top" alt={`Avatar of ${user.first_name}`} />
+                  <div className="card-body">
+                    <h5 className="card-title">{`${user.first_name} ${user.last_name}`}</h5>
+                    <p className="card-text">{user.email}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
-        )
+        </div>
       )}
     </div>
   );
